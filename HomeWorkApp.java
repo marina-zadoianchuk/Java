a665c26c5df34fc3ea5c62bd949763177c1f05e5
marina-zadoianchuk <93602034+marina-zadoianchuk@users.noreply.github.com>
2022-04-11 12:23:32 +0300

    Add files via upload

---
+++ HomeWorkApp.java
@@ -0,0 +1,52 @@
+package ru.gb.java.homework1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -30;
+        int b = 15;
+        //int c = a + b;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = -15;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = -15;
+        int b = 8;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+}
+
